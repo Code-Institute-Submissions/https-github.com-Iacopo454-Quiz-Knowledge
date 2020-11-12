0761943f159f6a0dfb49ff99752d773169dcd75e
Iacopo454 <iacopo.gentile@icloud.com>
2020-11-12 15:20:00 +0000

    I have created a google map of all the most important italian cities all over the country, I have read and follow the documentation for google map MarketCluster and after I made sure that it was working I have enter inside the Javascrpit code all the coordinates for each city I wanted to mark, I find this with a google search

---
+++ mail.js
@@ -0,0 +1,29 @@
+const btn = document.getElementById('button');
+
+
+document.getElementById('form')
+ .addEventListener('submit', function(event) {
+   event.preventDefault();
+
+   btn.value = 'Sending...';
+
+   const serviceID = 'default_service';
+   const templateID = 'testing';
+
+   emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+      btn.value = 'Send Email';
+      alert('Sent!');
+    }, (err) => {
+      btn.value = 'Send Email';
+      alert(JSON.stringify(err));
+    });
+});
+
+
+
+(function() {
+emailjs.init("user_5u9yIZ0m0BaHHQwavMgbq");
+})();
+
+
